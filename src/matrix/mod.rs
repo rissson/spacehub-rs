0b3a46956cc02ac68399873c79beb4c2de92aefd
MTRNord <mtrnord1@gmail.com>
2021-02-21 22:42:23 +0100

    feat: prepare general space creation

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,6 +1,17 @@
 use crate::Config;
 use color_eyre::eyre::Result;
-use ruma::RoomId;
+use ruma::{
+    api::client::r0::room::{create_room, Visibility},
+    events::{
+        room::{
+            avatar::AvatarEventContent,
+            history_visibility::{HistoryVisibility, HistoryVisibilityEventContent},
+            power_levels::PowerLevelsEventContent,
+        },
+        AnyInitialStateEvent, InitialStateEvent,
+    },
+    int, RoomId,
+};
 use ruma_client::Client;
 use tracing::*;
 
@@ -23,10 +34,57 @@ impl Matrix {
     }
 
     pub async fn create_space(
+        &self,
         name: String,
         description: Option<String>,
         avatar_url: Option<String>,
     ) {
+        let space = create_room::Request {
+            creation_content: create_room::CreationContent {
+                federate: true,
+                predecessor: None,
+                room_type: Some("".to_string()),
+            },
+            initial_state: &vec![
+                AnyInitialStateEvent::RoomHistoryVisibility(InitialStateEvent {
+                    state_key: "".to_string(),
+                    content: HistoryVisibilityEventContent {
+                        history_visibility: HistoryVisibility::Invited,
+                    },
+                }),
+                AnyInitialStateEvent::RoomAvatar(InitialStateEvent {
+                    state_key: "".to_string(),
+                    content: AvatarEventContent {
+                        info: None,
+                        url: avatar_url.unwrap_or_default(),
+                    },
+                }),
+            ],
+            name: Some(&name),
+            preset: Some(create_room::RoomPreset::PrivateChat),
+            invite: &vec![],
+            invite_3pid: &vec![],
+            is_direct: false,
+            room_alias_name: None,
+            room_version: None,
+            topic: description.as_ref().map(|x| &**x),
+            visibility: Visibility::Private,
+            power_level_content_override: Some(
+                PowerLevelsEventContent {
+                    events_default: int!(100),
+                    ..Default::default()
+                }
+                .into(),
+            ),
+        };
+
+        let room = match self.client.request(space).await {
+            Ok(a) => a,
+            _ => {
+                error!("Failed to create space");
+                return;
+            }
+        };
     }
     pub async fn set_child(parent_id: RoomId, child_id: RoomId) {}
     pub async fn get_spaces_toplevel() {}
